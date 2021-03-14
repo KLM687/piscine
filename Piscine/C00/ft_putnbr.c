aa33d16696001a705aec360cbaae25928457922d
cesco <cesco0724@gmail.com>
2021-03-14 15:42:29 -0400

    modif putnbr

--- Piscine/C00/ft_putnbr.c
+++ Piscine/C00/ft_putnbr.c
@@ -5,37 +5,36 @@ void ft_putchar(char c)
 	write ( 1 , &c , 1 );
 }
 
-
-
-
-
-
-
-
-
-
-void ft_putnbr ( int nb )
+void	ft_putnbr(int nb)
 {
-	if ( nb >= 0 && nb < 10 )
+	long int a;
+
+	a = nb;
+	if (a< 0)
 	{
-		ft_putchar( nb + '0');
+		ft_putchar('-');
+		a = -a;
 	}
-	else if ( nb < 0 ) 
+	if (a > 9)
 	{
-		ft_putchar( '-' );
-		ft_putnbr( nb * (-1));
+		ft_putnbr(a / 10);
+		ft_putnbr(a % 10);
 	}
 	else
-	{
-		ft_putnbr ( nb / 10 );
-		ft_putnbr ( nb % 10 );
-	}
+		ft_putchar(a + '0');
 }
+
+
+
+
+
+
+
 
 int main ()
 {
 	int nb;
-	nb = 1234;
+	nb = -2147483648;
 	ft_putnbr(nb);
 return 0;
 }
