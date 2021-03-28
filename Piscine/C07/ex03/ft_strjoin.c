7baa113307f0e3649c2627d087e442b6c009d73b
cesco <cesco0724@gmail.com>
2021-03-28 11:08:34 -0400

    boum

--- Piscine/C07/ex03/ft_strjoin.c
+++ Piscine/C07/ex03/ft_strjoin.c
@@ -1,86 +1,73 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int		ft_strlen(char *str)
 {
-	char	*charptr;
-	int		iterator;
-
-	charptr = str;
-	iterator = 0;
-	while (*charptr != 0)
-	{
-		iterator++;
-		charptr++;
-	}
-	return (iterator);
-}
-
-void	printsep(char **concatptr, char *sep)
-{
-	while (*sep)
-	{
-		**concatptr = *sep;
-		sep++;
-		*concatptr += 1;
-	}
-}
-
-void	makeconcat(int size, char *concatptr, char **strs, char *sep)
-{
-	int n;
 	int i;
 
-	n = 0;
-	while (n < size)
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int		ft_malloc_size(char **strs, int size, char *sep)
+{
+	int i;
+	int len;
+
+	i = 0;
+	len = 0;
+	while (i < size)
 	{
-		i = 0;
-		while (strs[n][i])
-		{
-			*concatptr = strs[n][i];
-			concatptr++;
-			i++;
-		}
-		if (n == size - 1)
-			*concatptr = 0;
-		else if (*sep)
-			printsep(&concatptr, sep);
-		n++;
+		len = len + ft_strlen(strs[i]);
+		i++;
 	}
+	len = len + (size - 1) * ft_strlen(sep) + 1;
+	return (len);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i])
+		i++;
+	while (src[j])
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = 0;
+	return (dest);
 }
 
 char	*ft_strjoin(int size, char **strs, char *sep)
 {
-	char	*concat;
-	char	*concatptr;
-	int		nbchar;
+	int		i;
+	char	*str;
 
-	if (size)
+	i = 0;
+	if (size == 0)
 	{
-		nbchar = (strs[size - 1] - *strs) + ft_strlen(strs[size - 1]) + 1;
-		concat = (char *)malloc(nbchar + ((ft_strlen(sep) - 1) * (size - 1)));
+		str = malloc(sizeof(char));
+		*str = 0;
+		return (str);
 	}
-	else
+	if (!(str = malloc(sizeof(char) * ft_malloc_size(strs, size, sep))))
+		return (NULL);
+	*str = 0;
+	while (i < size)
 	{
-		concat = malloc(1);
-		*concat = 0;
-		return (concat);
+		ft_strcat(str, strs[i]);
+		if (i < size - 1)
+			ft_strcat(str, sep);
+		i++;
 	}
-	concatptr = concat;
-	makeconcat(size, concatptr, strs, sep);
-	return (concat);
+	return (str);
 }
 
-
-int main()
-{
-	char *tablo[6];
-	tablo[0] = "coucou";
-	tablo[1] = "bite";
-	tablo[2] = "fesse";
-	tablo[3] = "de";
-	tablo[4] = "la";
-	tablo[5] = "merde";
-	printf("%s\n", ft_strjoin(6, tablo, "!"));
-	return (0);
-}
